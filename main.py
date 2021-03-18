d415ae60d8c11cb98cf97f38fc4ce302d108182e
Kendohu <62639622+CoalByte@users.noreply.github.com>
2021-03-18 14:34:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ most_frequent = lambda data: max(set(data), key=data.count) # Define a function 
 
 while data.__len__() != 1:  # External Loop (equivalent to [:-1]?)
     options = [s for value in data.values() for s in value.keys()] # Get all characteristics
-    try:
-        possible_options = [data[key][most_frequent(options)] for key in data.keys() if data[key][most_frequent(options)]] # Get all different values for the characteristic with most instances in "options"
-    except ValueError:
-        break
+    possible_options = [data[key][most_frequent(options)] for key in data.keys() if data[key][most_frequent(options)]] # Get all different values for the characteristic with most instances in "options"
 
     print("Answer options:", ', '.join(possible_options)) # Print the possible options
     data1 = input(f"What/where is/are/Can your character('s) {most_frequent(options)}? ") # Get data for the option we are asking about
