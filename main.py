c8b2ebef2450c9ea97e2aeac6a61639ead50c7af
Kendohu <62639622+CoalByte@users.noreply.github.com>
2021-03-18 14:23:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
 
-data = json.loads(open("dataset.txt", "r").read()) # Open and load dataset as a dictionary
+data = json.loads(open("dataset.json", "r").read()) # Open and load dataset as a dictionary
 
 most_frequent = lambda data: max(set(data), key=data.count) # Define a function to get the most common value out of a list
 
@@ -10,7 +10,7 @@ while data.__len__() != 1:  # External Loop (equivalent to [:-1]?)
     possible_options = [data[key][most_frequent(options)] for key in data.keys() if data[key][most_frequent(options)]] # Get all different values for the characteristic with most instances in "options"
 
     print("Answer options:", ', '.join(possible_options)) # Print the possible options
-    data1 = input(f"What/where is/are your character('s) {most_frequent(options)}? ") # Get data for the option we are asking about
+    data1 = input(f"What/where is/are/Can your character('s) {most_frequent(options)}? ") # Get data for the option we are asking about
 
     for key in [key for key in data.keys() if data[key][most_frequent(options)] != data1]: # Get every option with a value that doesn't match the value we have been given
         del data[key] # Remove that option
