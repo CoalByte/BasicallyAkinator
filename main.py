53d175cd9ae46be221385fb9c454c4b0cad014c3
Kendohu <62639622+CoalByte@users.noreply.github.com>
2021-03-18 13:47:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import json
 
 
-data = json.loads(open("dataset.txt", "r").read())
+data = json.loads(open("dataset.txt", "r").read()) # Open and load dataset as a dictionary
 
-most_frequent = lambda data: max(set(data), key=data.count)
+most_frequent = lambda data: max(set(data), key=data.count) # Define a function to get the most common value out of a list
 
-while data[:-1]:
-    options = [s for value in data.values() for s in value.keys()]
-    possible_options = [data[key][most_frequent(options)] for key in data.keys() if data[key][most_frequent(options)]]
+while data[:-1]: # Loop (equivallent to while data.__len__() != 1)
+    options = [s for value in data.values() for s in value.keys()] # Get all characteristics
+    possible_options = [data[key][most_frequent(options)] for key in data.keys() if data[key][most_frequent(options)]] # Get all different values for the characteristic with most instances in "options"
 
-    print("Answer options:", ', '.join(possible_options))
-    data1 = input(f"What/where is/are your character('s) {most_frequent(options)}? ")
+    print("Answer options:", ', '.join(possible_options)) # Print the possible options
+    data1 = input(f"What/where is/are your character('s) {most_frequent(options)}? ") # Get data for the option we are asking about
 
-    for key in [key for key in data.keys() if data[key][most_frequent(options)] != data1]:
-        del data[key]
+    for key in [key for key in data.keys() if data[key][most_frequent(options)] != data1]: # Get every option with a value that doesn't match the value we have been given
+        del data[key] # Remove that option
 
 
-print("You guessed", list(data.keys())[0])
+print("You guessed", list(data.keys())[0]) # Print the remaining option
